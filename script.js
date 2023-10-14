812831b4a2aa0b71f251ca4a284416491608defc
Christian <explosivfibres@hotmail.com>
2023-10-14 19:06:04 +0200

    third

--- script.js
+++ script.js
@@ -38,43 +38,45 @@ if (document.body.classList.contains("body__index--js")) {
 }
 
 // Var for the stars to delete some html.
-var article__text = [
-  "Pick your hotel with us, 5 stars with high-class rooms! We offer something to remember for life. Spa, pools, and more.",
-  "Our hotel is the perfect destination for your next vacation! Luxurious accommodations, gourmet dining.",
-  "Experience the ultimate relaxation at our spa resort. Unwind in our serene surroundings and enjoy world-class facilitys.",
-  "Our friendly staff is dedicated to making your stay memorable. We go the extra mile to ensure your comfort.",
-  "Our hotel exceeded all my expectations! The service was impeccable, and the food was divine.",
-  "I had the most relaxing time at this hotel. The spa treatments were heavenly, and the staff was incredibly attentive.",
-  "Staying here was a dream come true. The room had a stunning view, and the amenities were top-notch.",
-  "This hotel made our honeymoon unforgettable. The romantic atmosphere and beautiful surroundings were perfect.",
-  "I've never experienced such luxury before. This hotel sets a new standard for excellence in hospitality.",
-  "The entire family loved our stay. There were activities for everyone, from the kids to the adults.",
-  "The attention to detail at this hotel is remarkable. Every aspect of our stay was flawless.",
-];
+if (document.body.classList.contains("body__index--js")) {
+  var article__text = [
+    "Pick your hotel with us, 5 stars with high-class rooms! We offer something to remember for life. Spa, pools, and more.",
+    "Our hotel is the perfect destination for your next vacation! Luxurious accommodations, gourmet dining.",
+    "Experience the ultimate relaxation at our spa resort. Unwind in our serene surroundings and enjoy world-class facilitys.",
+    "Our friendly staff is dedicated to making your stay memorable. We go the extra mile to ensure your comfort.",
+    "Our hotel exceeded all my expectations! The service was impeccable, and the food was divine.",
+    "I had the most relaxing time at this hotel. The spa treatments were heavenly, and the staff was incredibly attentive.",
+    "Staying here was a dream come true. The room had a stunning view, and the amenities were top-notch.",
+    "This hotel made our honeymoon unforgettable. The romantic atmosphere and beautiful surroundings were perfect.",
+    "I've never experienced such luxury before. This hotel sets a new standard for excellence in hospitality.",
+    "The entire family loved our stay. There were activities for everyone, from the kids to the adults.",
+    "The attention to detail at this hotel is remarkable. Every aspect of our stay was flawless.",
+  ];
 
-// Array loop
-var elements = document.getElementsByClassName("article__text");
+  // Array loop
+  var elements = document.getElementsByClassName("article__text");
 
-for (var i = 0; i < elements.length; i++) {
-  var randomIndex = Math.floor(Math.random() * article__text.length);
-  elements[i].innerText = article__text[randomIndex];
-}
-
-// Get all elements with the class "star-container"
-var starContainers = document.querySelectorAll(".article__stars");
-
-// Function to generate a random number of active stars
-function getRandomNumActiveStars() {
-  return Math.floor(Math.random() * 5) + 1; // Random number of active stars between 1 and 5
-}
-
-// Loop through each star container
-starContainers.forEach(function (starContainer) {
-  var numActiveStars = getRandomNumActiveStars();
-
-  for (var i = 0; i < 5; i++) {
-    var star = document.createElement("div");
-    star.className = "star" + (i < numActiveStars ? " active" : "");
-    starContainer.appendChild(star);
+  for (var i = 0; i < elements.length; i++) {
+    var randomIndex = Math.floor(Math.random() * article__text.length);
+    elements[i].innerText = article__text[randomIndex];
   }
-});
+
+  // Get all elements with the class "star-container"
+  var starContainers = document.querySelectorAll(".article__stars");
+
+  // Function to generate a random number of active stars
+  function getRandomNumActiveStars() {
+    return Math.floor(Math.random() * 5) + 1; // Random number of active stars between 1 and 5
+  }
+
+  // Loop through each star container
+  starContainers.forEach(function (starContainer) {
+    var numActiveStars = getRandomNumActiveStars();
+
+    for (var i = 0; i < 5; i++) {
+      var star = document.createElement("div");
+      star.className = "star" + (i < numActiveStars ? " active" : "");
+      starContainer.appendChild(star);
+    }
+  });
+}
