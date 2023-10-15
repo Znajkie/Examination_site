f58ae2ff87328efde40b08ae6ececf13f6f9c638
Christian <explosivfibres@hotmail.com>
2023-10-15 20:14:15 +0200

    laast commit

--- script-room.js
+++ script-room.js
@@ -3,13 +3,12 @@ if (document.body.classList.contains("body__room--js")) {
   let currentSlide = 0;
 
   function roll() {
-    hero2[currentSlide].style.opacity = 0; // Hide the current slide
-    currentSlide = (currentSlide + 1) % 2; // Move to the next slide (there are only 2)
-    hero2[currentSlide].style.opacity = 1; // Show the next slide
+    hero2[currentSlide].style.opacity = 0;
+    currentSlide = (currentSlide + 1) % 2;
+    hero2[currentSlide].style.opacity = 1;
 
-    setTimeout(roll, 10000); // Adjust the delay as needed
+    setTimeout(roll, 10000);
 
-    // Initially show the first slide
     hero2[currentSlide].style.opacity = 1;
   }
   roll();
